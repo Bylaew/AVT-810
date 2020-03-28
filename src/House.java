8dabec2dcbf227e065cb173837c45b68dd9c16fd
spankyman125 <spankyman@live.ru>
2020-03-28 16:45:41 +0600

    IBehaviour

--- src/House.java
+++ src/House.java
@@ -1,23 +1,24 @@
 import java.awt.*;
 
-public abstract class House //все домики
+public abstract class House implements IBehaviour //все домики
 {
-    int x; //координаты
-    int y;
+    public int x; //координаты
+    public int y;
     public abstract Image getImage();
 
+    @Override
     public void setX(int x) {
         this.x = x;
     }
-
+    @Override
     public void setY(int y) {
         this.y = y;
     }
-
+    @Override
     public int getX() {
         return x;
     }
-
+    @Override
     public int getY() {
         return y;
     }
--- src/IBehaviour.java
+++ src/IBehaviour.java
@@ -1,2 +1,7 @@
-public class IBehaviour {
+public interface IBehaviour
+{
+    int getX();
+    int getY();
+    void setX(int x);
+    void setY(int y);
 }
